6271501ffbffbf4b18235a061216f4ea3c007a2e
iDebonair <ofemiowolabi@gmail.com>
2023-05-16 02:43:37 +0000

    Client.js was created and modified in line with ES6 shorthand synthax to export the connect function

---
+++ client.js
@@ -0,0 +1,20 @@
+const net = require("net");
+const connect = function () {
+  const conn = net.createConnection({
+    host: '10.0.2.15', // IP address here,
+    port: 50541 // PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  conn.on("data", (data) => {
+    console.log("Server says: ", data);
+  });
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
+
+module.exports = connect;
--- play.js
+++ play.js
@@ -1,22 +1,4 @@
-const net = require("net");
+const connect  = require("./client.js");
+
 
 // establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '10.0.2.15', // IP address here,
-    port: 50541 // PORT number here,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  conn.on("data", (data) => {
-    console.log("Server says: ", data);
-  });
-
-  return conn;
-};
-
-
-
-console.log("Connecting ...");
-connect();
