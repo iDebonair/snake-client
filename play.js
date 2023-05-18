a97fd469d323ac1bbec8ca1a72877ac1dbc231b7
iDebonair <ofemiowolabi@gmail.com>
2023-05-18 00:15:10 +0000

    setUpInput and handleUserInput functions were created with which to control the snake

--- play.js
+++ play.js
@@ -1,19 +1,9 @@
 const net = require("net");
-const connect  = require("./client.js");
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-const handleUserInput = function (key) {
-  if (key === '\u0003') {
-    process.exit(); // Exit the program if the 'Ctrl + C' key combination is pressed
-  }
-};
-
+const {setupInput} = require("./input.js");
+const {connect}  = require("./client.js");
 // establishes a connection with the game server
+
+console.log("Connecting ...");
+connect();
+
+setupInput();
